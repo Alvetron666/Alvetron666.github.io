2b6bc72d02451bd606f1deff39aeaebdd935ab89
Alvetron666 <38918732+Alvetron666@users.noreply.github.com>
2021-05-06 14:32:37 +0300

    commit 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,3 @@
-let tabsBtn = document.querySelectorAll(".advantage-btn");
-let services = document.querySelectorAll(".services-desc-item");
-
-
-tabsBtn.forEach(function(i){
-    i.addEventListener("click", function(evt){
-        evt.preventDefault();
-        btn = i;
-        currentBtn = i.getAttribute("data-btn");
-        
-
-        services.forEach(function(k){
-            k.classList.remove("active");
-        })
-
-        services.forEach(function(k){
-            let currentTab = k.getAttribute("data-btn");
-            
-           if(currentTab === currentBtn){
-               
-               k.classList.add("active");
-           }
-        })
-    })
-})
-
-
-let sliderBtns = document.querySelectorAll(".slider-btn");
-let sliderItems = document.querySelectorAll(".slider-item");
-
-
-sliderBtns.forEach(function(i){
-    i.addEventListener("click", function(){
-        sliderBtns.forEach(function(k){
-            k.classList.remove("active");
-        })
-        i.classList.add("active");
-        currentBtn = i.getAttribute("data-slider-btn");
-        
-
-
-        sliderItems.forEach(function(k){
-                k.classList.remove("active");  
-        })
-        sliderItems.forEach(function(k){
-            currentItem = k.getAttribute("data-slider-item");
-            if(currentItem === currentBtn){
-                k.classList.add("active");
-                
-            }
-           
-        })
-    })
-})
-
-
 let catBtn = document.querySelector(".catalogue-btn");
 let hiddenMenu =  document.querySelector(".catalog-hidden");
 
@@ -62,9 +6,11 @@ catBtn.addEventListener("click", function(evt){
     
     if(! hiddenMenu.classList.contains("active")){
         hiddenMenu.classList.add("active");
+        hiddenMenu.querySelector(".catalog-list li a").focus()
  
     }else{
         hiddenMenu.classList.remove("active");
+        catBtn.focus();
     }
 })
 
--- assets/js/modal-contact.js
+++ assets/js/modal-contact.js
@@ -37,4 +37,59 @@ contactCloseBtns.forEach(function(i){
 
 
 
+let sliderBtns = document.querySelectorAll(".slider-btn");
+let sliderItems = document.querySelectorAll(".slider-item");
 
+
+sliderBtns.forEach(function(i){
+    i.addEventListener("click", function(){
+        sliderBtns.forEach(function(k){
+            k.classList.remove("active");
+        })
+        i.classList.add("active");
+        currentBtn = i.getAttribute("data-slider-btn");
+        
+
+
+        sliderItems.forEach(function(k){
+                k.classList.remove("active");  
+        })
+        sliderItems.forEach(function(k){
+            currentItem = k.getAttribute("data-slider-item");
+            if(currentItem === currentBtn){
+                k.classList.add("active");
+                
+            }
+           
+        })
+    })
+})
+
+
+
+
+let tabsBtn = document.querySelectorAll(".advantage-btn");
+let services = document.querySelectorAll(".services-desc-item");
+
+
+tabsBtn.forEach(function(i){
+    i.addEventListener("click", function(evt){
+        evt.preventDefault();
+        btn = i;
+        currentBtn = i.getAttribute("data-btn");
+        
+
+        services.forEach(function(k){
+            k.classList.remove("active");
+        })
+
+        services.forEach(function(k){
+            let currentTab = k.getAttribute("data-btn");
+            
+           if(currentTab === currentBtn){
+               
+               k.classList.add("active");
+           }
+        })
+    })
+})
