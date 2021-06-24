c6577efde656699240634b51ce07e5e770500a8e
Alvetron666 <thesparkylegendary@gmail.com>
2021-06-24 11:51:51 +0300

    comm6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,12 @@
 let heartAnim = document.querySelector('footer .container');
 let heartAnimPos = heartAnim.offsetTop;
 let heartimg = document.querySelector('.heart__icon');
-console.log(heartimg)
+
 window.addEventListener('scroll', function(){
     windowPos = +window.scrollY + +window.innerHeight;
-    console.log(windowPos)
     if(windowPos >= heartAnimPos){
         heartimg.classList.add('active');
-        window.offsetTop = window.offsetTop;
-        
-
+    }else{
+        heartimg.classList.remove('active'); 
     }
 })
