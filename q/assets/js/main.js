aaf2e72c3ab5d0b8f877b3aa1469e745fde9865e
Alvetron666 <thesparkylegendary@gmail.com>
2021-07-04 14:15:57 +0300

    1

--- q/assets/js/main.js
+++ q/assets/js/main.js
@@ -1,3 +1,33 @@
+function fixOnScroll(){
+    let header =  document.querySelector(".main__header");
+    let headerHeight = header.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        let windowPos = window.scrollY;
+        if(windowPos > headerHeight){
+            header.classList.add('active');
+        }else{
+            header.classList.remove('active');
+        }
+    })
+}
+
+function burderMenu(){
+    let burgerBtn = document.querySelector('.burger__menu');
+    let linksList = document.querySelector('.links__list');
+
+    burgerBtn.onclick = () => {
+        if( ! burgerBtn.classList.contains('active') ){
+            burgerBtn.classList.add('active');
+            linksList.style.display = 'flex';
+        }else{
+            burgerBtn.classList.remove('active');
+            linksList.style.display = 'none';
+        }
+        
+    }
+}
+
 function scrollToHandler(){
     window.scrollTo({
         top: 0,
@@ -5,3 +35,6 @@ function scrollToHandler(){
         behavior: 'smooth',
     });
 }
+
+fixOnScroll();
+burderMenu();
