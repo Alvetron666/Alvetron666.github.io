165436ea7608135ceb39c4407ceb8ca74999f833
Alvetron <thesparkylegendary@gmail.com>
2021-10-02 21:19:06 +0300

    com4

---
+++ queen/assets/js/form.js
@@ -0,0 +1,24 @@
+"use sctrict"
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('quiz-form');
+
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+        let formData = new FormData(form);
+
+        
+            let response = await fetch('/../../php/mail.php', {
+                method: 'POST',
+                body: formData,
+            });
+            if (response.ok) {
+                let result = await response.json();
+                form.reset();
+            } else {
+                alert('Ошибка');
+            }
+   
+    }
+});
